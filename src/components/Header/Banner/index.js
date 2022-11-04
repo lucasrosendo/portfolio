e2fcff2c555911aebb11c0b73f681c2527bf906d
lucasrosendo <lucasrosendo91@gmail.com>
2022-11-04 03:50:54 -0300

    em construcao

--- src/components/Header/Banner/index.js
+++ src/components/Header/Banner/index.js
@@ -6,7 +6,7 @@ export default function Banner() {
   return (
     <div className='banner'>
       <div className='content-banner'>
-
+        <h1>EM CONSTRUÇÃO!!!</h1>
         <h2>Texto aqui</h2>
         <h1>texto aqui</h1>
         <p>adfsdfsdfsdfsdfsdf</p>
